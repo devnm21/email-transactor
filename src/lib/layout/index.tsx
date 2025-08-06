187a19bfab3195c9d6583d5159d296c73324224d
Devakumar NM <>
2025-08-06 23:17:39 +0530

    boilerpalte cleanup

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -12,7 +12,7 @@ type LayoutProps = {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
+    <Box margin="0 auto" transition="0.5s ease-out">
       <Box margin="8">
         <Header />
         <Box as="main" marginY={22}>
